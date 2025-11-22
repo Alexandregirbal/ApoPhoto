b79c9e2f17ebb49610dcb0778d499ca9a5ec5af4
Alexandre Girbal <alexandre.girbal.pro@gmail.com>
2025-11-22 10:29:22 +0100

    init sveltkit project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import { m } from '$lib/paraglide/messages.js';
+</script>
+
+<h1>{m['pages.home.hello_world']()}</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
